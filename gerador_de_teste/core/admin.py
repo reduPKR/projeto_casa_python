964eb46c7695719c3b297f0d9b26cea2e7914ae6
redupkr <reduardo1994@hotmail.com>
2020-07-26 22:11:37 -0300

    Subindo banco registrado no admin

--- gerador_de_teste/core/admin.py
+++ gerador_de_teste/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import *
 
 # Register your models here.
+admin.site.register(Tipo)
+admin.site.register(Saida)
+admin.site.register(Equipamento)
+admin.site.register(Casa)
+admin.site.register(Comodo)
+admin.site.register(ComodoSaida)
+admin.site.register(Categoria)
+admin.site.register(ConsumoHora)
+admin.site.register(ConsumoMes)
+admin.site.register(MesHora)
